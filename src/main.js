9061cc68ed5f9979fdd7c94465f22e6ff6c959d6
Oleg Peunov <olegpeunov@yandex.ru>
2022-01-17 17:47:48 +0300

    HTML is done

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.root');
+const form = container.querySelector('#exampleModal');
+
+// document.onreadystatechange = function () {
+// if (document.readyState == "complete") {
+    
+//     console.log(form);
+//     form.modal('show')
+// }
+// }
+
