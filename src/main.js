dac7294f55fa9a8b9cee042cac84e7d1fadd0a3f
Oleg Peunov <olegpeunov@yandex.ru>
2022-01-17 18:24:56 +0300

    Jq and Popup are added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-const container = document.querySelector('.root');
-const form = container.querySelector('#exampleModal');
+
+
+$(document).ready(function(){
+    $(window).on('load',function(){
+        $('#exampleModal').modal('show')
+    })
+});
+
+
+// const container = document.querySelector('.root');
+// const form = container.querySelector('#exampleModal');
+
+// console.log('test')
+
+// $(window).on('load',function(){
+//     $('#myModal').modal('show');
+// });
 
 // document.onreadystatechange = function () {
 // if (document.readyState == "complete") {
